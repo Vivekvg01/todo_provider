66c4e1449254480a84aa6a8d61859a3e6d70150b
Vivek VG <vivekvggit@gmail.com>
2023-09-11 00:14:34 +0530

    CRUD Completed

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_provider/pages/todo_pages.dart';
+import 'package:todo_provider/providers/providers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoFilter>(
+          create: (context) => TodoFilter(),
+        ),
+        ChangeNotifierProvider<TodoSearch>(
+          create: (context) => TodoSearch(),
+        ),
+        ChangeNotifierProvider<TodoList>(
+          create: (context) => TodoList(),
+        ),
+        ChangeNotifierProxyProvider<TodoList, ActiveTodoCount>(
+          create: (context) => ActiveTodoCount(
+            initialActiveTodoCount: context.read<TodoList>().state.todos.length,
+          ),
+          update: (BuildContext context, TodoList todoList,
+                  ActiveTodoCount? activeTodoCount) =>
+              activeTodoCount!..update(todoList),
+        ),
+        ChangeNotifierProxyProvider3<TodoFilter, TodoSearch, TodoList,
+            FilteredTodos>(
+          create: (context) => FilteredTodos(
+              initialFilteredTodos: context.read<TodoList>().state.todos),
+          update: (BuildContext context,
+                  TodoFilter todoFilter,
+                  TodoSearch todoSearch,
+                  TodoList todoList,
+                  FilteredTodos? filteredTodos) =>
+              filteredTodos!..update(todoFilter, todoSearch, todoList),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TodosPage(),
+      ),
+    );
+  }
+}
---
+++ lib/pages/todo_pages.dart
@@ -0,0 +1,314 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_provider/models/todo_model.dart';
+import 'package:todo_provider/providers/providers.dart';
+import 'package:todo_provider/utils/debounce.dart';
+
+class TodosPage extends StatefulWidget {
+  const TodosPage({super.key});
+
+  @override
+  State<TodosPage> createState() => _TodosPageState();
+}
+
+class _TodosPageState extends State<TodosPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 20.0,
+              horizontal: 40.0,
+            ),
+            child: Column(
+              children: [
+                TodoHeader(),
+                CreateTodo(),
+                SizedBox(height: 20.0),
+                SearchAndFilterTodo(),
+                SizedBox(height: 10.0),
+                ShowTodos(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TodoHeader extends StatelessWidget {
+  const TodoHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'TODO',
+          style: TextStyle(fontSize: 40.0),
+        ),
+        Text(
+          '${context.watch<ActiveTodoCount>().state.activeTodoCount} items left',
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.redAccent,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CreateTodo extends StatefulWidget {
+  CreateTodo({super.key});
+
+  @override
+  State<CreateTodo> createState() => _CreateTodoState();
+}
+
+class _CreateTodoState extends State<CreateTodo> {
+  final newTodoController = TextEditingController();
+  @override
+  void dispose() {
+    newTodoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: newTodoController,
+      decoration: InputDecoration(
+        label: Text('What to do?'),
+      ),
+      onSubmitted: (String? todoDesc) {
+        if (todoDesc != null && todoDesc.trim().isNotEmpty) {
+          context.read<TodoList>().addTodo(todoDesc);
+          newTodoController.clear();
+        }
+      },
+    );
+  }
+}
+
+class SearchAndFilterTodo extends StatelessWidget {
+  SearchAndFilterTodo({super.key});
+
+  final debounce = Debounce(milliseconds: 1000);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            label: Text('Search Todo'),
+            border: InputBorder.none,
+            filled: true,
+            prefixIcon: Icon(Icons.search),
+          ),
+          onChanged: (String? newSearchTerm) {
+            if (newSearchTerm != null) {
+              debounce.run(() {
+                context.read<TodoSearch>().searchTerm(newSearchTerm);
+              });
+            }
+          },
+        ),
+        SizedBox(height: 10.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            filterButton(context, Filter.all),
+            filterButton(context, Filter.active),
+            filterButton(context, Filter.completed),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget filterButton(BuildContext context, Filter filter) {
+    return TextButton(
+      onPressed: () {
+        context.read<TodoFilter>().changeFilter(filter);
+      },
+      child: Text(
+        filter == Filter.all
+            ? 'All'
+            : filter == Filter.active
+                ? 'Active'
+                : 'Completed',
+        style: TextStyle(
+          fontSize: 18.0,
+          color: textColor(context, filter),
+        ),
+      ),
+    );
+  }
+
+  Color textColor(BuildContext context, Filter filter) {
+    final currentFilter = context.watch<TodoFilter>().state.filter;
+    return currentFilter == filter ? Colors.blue : Colors.grey;
+  }
+}
+
+class ShowTodos extends StatelessWidget {
+  const ShowTodos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final todos = context.watch<FilteredTodos>().state.filteredTodos;
+
+    Widget showBackgound(int direction) {
+      return Container(
+        margin: EdgeInsets.all(4.0),
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        color: Colors.red,
+        alignment:
+            direction == 0 ? Alignment.centerLeft : Alignment.centerRight,
+        child: Icon(
+          Icons.delete,
+          size: 30.0,
+          color: Colors.white,
+        ),
+      );
+    }
+
+    return ListView.separated(
+      primary: false,
+      shrinkWrap: true,
+      itemBuilder: (BuildContext context, int index) {
+        log(todos.length.toString());
+        return Dismissible(
+          key: ValueKey(todos[index].id),
+          background: showBackgound(0),
+          secondaryBackground: showBackgound(1),
+          onDismissed: (_) {
+            context.read<TodoList>().removeTodo(todos[index]);
+          },
+          confirmDismiss: (_) {
+            return showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text('Are you sure?'),
+                  content: Text('Do you really want to delete?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context, false);
+                      },
+                      child: Text('NO'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context, true);
+                      },
+                      child: Text('Yes'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          child: TodoItem(todo: todos[index]),
+        );
+      },
+      separatorBuilder: (_, __) => Divider(),
+      itemCount: todos.length,
+    );
+  }
+}
+
+class TodoItem extends StatefulWidget {
+  final Todo todo;
+  const TodoItem({super.key, required this.todo});
+
+  @override
+  State<TodoItem> createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  late final TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            bool _error = false;
+            textEditingController.text = widget.todo.desc;
+            return StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+                return AlertDialog(
+                  title: Text('Edit Todo'),
+                  content: TextField(
+                    controller: textEditingController,
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      errorText: _error ? 'Value Cannot be empty' : null,
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Cancel'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          _error =
+                              textEditingController.text.isEmpty ? true : false;
+                        });
+
+                        if (!_error) {
+                          context.read<TodoList>().editTodo(
+                                widget.todo.id,
+                                textEditingController.text,
+                              );
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text('Edit'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        );
+      },
+      leading: Checkbox(
+        value: widget.todo.completed,
+        onChanged: (bool? checked) {
+          context.read<TodoList>().toggleTodo(widget.todo.id);
+        },
+      ),
+      title: Text(widget.todo.desc),
+    );
+  }
+}
